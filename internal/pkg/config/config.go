5d325db89df2ad202ecdf411afd5480afbc5b22f
zfkhhh <zengfk7213@gmail.com>
2022-07-15 21:35:59 +0800

    test

---
+++ internal/pkg/config/config.go
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2022, AcmeStack
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package config
+
+import (
+	"io/ioutil"
+	"log"
+
+	"gopkg.in/yaml.v3"
+)
+
+var (
+	ExchangerType = "Exchanger"
+	MysqlType     = "MySQL"
+)
+
+// mysql the MySQL config
+type mysql struct {
+	// Url with standard Url: mysql://user:123@localhost:123
+	Url string `yaml:"url"`
+}
+
+// Config the envcd config
+type Config struct {
+	// Exchanger with standard Url: etcd://user:123@localhost:123
+	// the schema is the kind of the center
+	Exchanger             string `yaml:"exchanger"`
+	ExchangerConnMetadata *ConnMetadata
+	Mysql                 *mysql `yaml:"mysql"`
+	MysqlConnMetadata     *ConnMetadata
+}
+
+// NewConfig new envcd config
+//  @param configFile the config file
+//  @return *Config current config instance
+func NewConfig(configFile *string) *Config {
+	data, err := ioutil.ReadFile(*configFile)
+	if err != nil {
+		log.Fatalf("error %v", err)
+	}
+	envcdConfig := &Config{}
+	if e := yaml.Unmarshal(data, envcdConfig); e != nil {
+		log.Fatalf("error %v", err)
+	}
+	return envcdConfig
+}
+
+// StartInformation the envcd config information
+//  @receiver cfg
+func (cfg *Config) StartInformation() {
+	cfg.ExchangerConnMetadata = parser(cfg.Exchanger)
+	cfg.ExchangerConnMetadata.information(ExchangerType)
+	cfg.MysqlConnMetadata = parser(cfg.Mysql.Url)
+	cfg.MysqlConnMetadata.information(MysqlType)
+}
